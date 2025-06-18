bdd56d62a3a45db9f73fcb965151d5583a25a702
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-06-18 20:20:40 -0300

    Eslint format

--- chinook/src/index.ts
+++ chinook/src/index.ts
@@ -1,12 +1,13 @@
-import express from 'express';
-import { router as homeRouter } from './routes/home';
-import { router as artistsRouter } from './routes/artists';
+import express from "express";
+import { router as homeRouter } from "./routes/home";
+import { router as artistsRouter } from "./routes/artists";
 const app = express();
 
 app.use(express.json());
-app.use('/artists', artistsRouter);
-app.use('/', homeRouter)
+app.use("/artists", artistsRouter);
+app.use("/", homeRouter);
 
 const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
-
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`),
+);
--- chinook/src/routes/home.ts
+++ chinook/src/routes/home.ts
@@ -1,9 +1,8 @@
+import express, { Request, Response } from "express";
 
-import express, { Request, Response } from 'express'
-
-export const router = express.Router()
+export const router = express.Router();
 
 // Index
-router.get('/', (_req: Request, res: Response) => {
-  res.send('Hello, World!')
-})
+router.get("/", (_req: Request, res: Response) => {
+  res.send("Hello, World!");
+});
